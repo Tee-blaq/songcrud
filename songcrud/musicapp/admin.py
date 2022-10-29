3c11edd25167e34e4bf92ffe643fefcfbdadd3b0
Tee_blaq <adekoyataiye38@gmail.com>
2022-10-29 10:03:02 +0100

    songcrud

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artiste,Song,Lyric
+
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
 
 # Register your models here.
